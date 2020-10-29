fe6f115baeb1e8a31809881410f803912be57cd2
Justin Movitz <movitzj1@montclair.edu>
2020-10-29 17:35:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -36,7 +36,7 @@ def game_hash
           :number => 2,
           :shoe => 14,
           :points => 24,
-          :reboubnds => 12,
+          :rebounds => 12,
           :assists => 12,
           :steals => 4,
           :blocks => 5,
