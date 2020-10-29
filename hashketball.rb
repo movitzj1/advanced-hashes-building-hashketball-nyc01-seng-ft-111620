7211a8a2be329837548c520adaaa77195081ead3
Justin Movitz <movitzj1@montclair.edu>
2020-10-29 17:28:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,42 +8,53 @@ def game_hash
       :colors => ["Turquoise", "Purple"]
 
       :players =>
-      [
-        {
-        :player_name => "Jeff Adrien",
-        :number => 4,
-        :shoe => 18,
-        :points => 10,
-        :reboubnds => 1,
-        :assists => 1,
-        :steals => 2,
-        :blocks => 7,
-        :slam_dunks => 2
-        },
-        {
-        :player_name => "Bismack Biyombo"
-        :number => 0
-        :shoe => 16
-        :points => 12
-        :reboubnds => 4
-        :assists => 7
-        :steals => 22
-        :blocks => 15
-        :slam_dunks => 10
-        },
-        {
-        :player_name => "DeSagna Diop",
-        :number => 2,
-        :shoe => 14,
-        :points => 24,
-        :reboubnds => 12,
-        :assists => 12,
-        :steals => 4,
-        :blocks => 5,
-        :slam_dunks => 5
-        },
+        [
+          {
+          :player_name => "Jeff Adrien",
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :reboubnds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2
+          },
+          {
+          :player_name => "Bismack Biyombo"
+          :number => 0
+          :shoe => 16
+          :points => 12
+          :reboubnds => 4
+          :assists => 7
+          :steals => 22
+          :blocks => 15
+          :slam_dunks => 10
+          },
+          {
+          :player_name => "DeSagna Diop",
+          :number => 2,
+          :shoe => 14,
+          :points => 24,
+          :reboubnds => 12,
+          :assists => 12,
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5
+          },
+          {
+          :player_name => "Jason Terry",
+          :number => 31,
+          :shoe => 15,
+          :points => 19,
+          :reboubnds => 2,
+          :assists => 2,
+          :steals => 4,
+          :blocks => 11,
+          :slam_dunks => 1
+          },
 
-    ]
+        ]
 
     }
     :home =>
