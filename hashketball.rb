40f407760ad73d0c9c1e60209ed06e4a8cf453e2
Justin Movitz <movitzj1@montclair.edu>
2020-10-29 17:12:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -34,15 +34,26 @@ def game_hash
       :players =>
       [
         {
-          :player_name =>
-          :number =>
-          :shoe =>
-          :points =>
-          :reboubnds =>
-          :assists =>
-          :steals =>
-          :blocks =>
-          :slam_dunks =>
+          :player_name => "Alan Anderson",
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :reboubnds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
+        },
+        {
+          :player_name => "Reggie Evans",
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :reboubnds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
         }
       ]
 
