da3a230eb21da9d41ed145b5cf5d47535cc72182
Justin Movitz <movitzj1@montclair.edu>
2020-10-29 17:15:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -55,6 +55,17 @@ def game_hash
           :blocks => 12,
           :slam_dunks => 7
         }
+        {
+          :player_name => "Brook Lopez",
+        :number => 11,
+        :shoe => 17,
+        :points => 17,
+        :reboubnds => 19,
+        :assists => 10,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 15
+        }
       ]
 
     }
