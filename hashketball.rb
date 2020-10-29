0ebe63fddb21df98a36fff0def294e0ddd48f0f6
Justin Movitz <movitzj1@montclair.edu>
2020-10-29 17:21:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,13 +3,23 @@ def game_hash
   {
     :away =>
     {
-      :team_name =>
+      :team_name => "Charlotte Hornets"
 
-      :colors =>
+      :colors => ["Purple"]
 
       :players =>
-
-    [
+      [
+        {
+        :player_name => "Jason Terry",
+        :number => 31,
+        :shoe => 15,
+        :points => 19,
+        :reboubnds => 2,
+        :assists => 2,
+        :steals => 4,
+        :blocks => 11,
+        :slam_dunks => 1
+        }
       {
         :player_name =>
         :number =>
