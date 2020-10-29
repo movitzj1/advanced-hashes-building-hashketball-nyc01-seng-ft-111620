54b73e533d16cae46183c65c059daf53a6cd238c
Justin Movitz <movitzj1@montclair.edu>
2020-10-29 17:30:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -43,16 +43,27 @@ def game_hash
           :slam_dunks => 5
           },
           {
-          :player_name => "Jason Terry",
-          :number => 31,
+          :player_name => "Ben Gordon",
+          :number => 8,
           :shoe => 15,
-          :points => 19,
-          :reboubnds => 2,
+          :points => 33,
+          :reboubnds => 3,
           :assists => 2,
-          :steals => 4,
-          :blocks => 11,
-          :slam_dunks => 1
+          :steals => 1,
+          :blocks => 1,
+          :slam_dunks => 0
           },
+          {
+          :player_name => "Kemba Walker",
+          :number => 33,
+          :shoe => 15,
+          :points => 6,
+          :reboubnds => 12,
+          :assists => 12,
+          :steals => 7,
+          :blocks => 5,
+          :slam_dunks => 11
+          }
 
         ]
 
